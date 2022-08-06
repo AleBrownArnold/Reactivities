535422ba8ce6799b57e1c1199f883b7157fad115
Alejandro Brown Arnold <abrown@lasegunda.com.ar>
2022-08-06 20:06:16 -0300

    End of Section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,12 +6,17 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+    <ToastContainer position='bottom-right' hideProgressBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path={'/*'}
@@ -32,6 +37,9 @@ const test = (location: any) => {
           <Route path='/activities/:id' element={<ActivityDetails/>}/>
           <Route key={location.key} path={'/createActivity'} element={<ActivityForm/>}/>
           <Route key={location.key} path={'/manage/:id'} element={<ActivityForm/>}/>
+          <Route path='/errors' element={<TestErrors/>}></Route>
+          <Route path='/server-error' element={<ServerError/>}></Route>
+          <Route path='*' element={<NotFound/>}></Route>
         </Routes>
       </Container>
     </>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.css'
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
-import { BrowserRouter } from 'react-router-dom';
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
+    <HistoryRouter history={history}>
       <App />
-    </BrowserRouter>
+    </HistoryRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
